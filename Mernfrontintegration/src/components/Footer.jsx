1e1b9af2761488a2c785d27222ebd1b42a5dce9b
VRaghav1806 <raghav.v2024aids@sece.ac.in>
2025-12-18 23:19:46 +0530

    Update Footer.jsx

--- Mernfrontintegration/src/components/Footer.jsx
+++ Mernfrontintegration/src/components/Footer.jsx
@@ -33,7 +33,7 @@ export default function Footer() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <span>Mon - Fri: 9:00 AM - 6:00 PM</span>
+                <span>Mon - Fri: 9:00 AM - 7:00 PM</span>
               </li>
             </ul>
           </div>
